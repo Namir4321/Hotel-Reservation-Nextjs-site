1cdb4a565a84d8c273cb3ef38c077c684e13951e
Namir4321 <97880446+Namir4321@users.noreply.github.com>
2024-08-20 22:51:11 +0530

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+
+let prisma;
 if (process.env.NODE_ENV === "production") {
-  const prisma = new PrismaClient();
+  prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
