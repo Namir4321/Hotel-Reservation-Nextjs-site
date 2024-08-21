3c42363394dd730eeab443d3ad60452671cf5c40
Namir4321 <97880446+Namir4321@users.noreply.github.com>
2024-08-21 22:25:37 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -11,22 +11,30 @@ export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.some((route) =>
+    new RegExp(`^${route}$`).test(nextUrl.pathname)
+  );
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
   if (isApiAuthRoute) {
     return null;
   }
   if (isAuthRoute) {
     if (isLoggedIn) {
+      console.log("1");
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
+    console.log("3");
+
     return null;
   }
   if (!isLoggedIn && !isPublicRoute) {
+    console.log("12");
+
     return Response.redirect(new URL("/signin", nextUrl));
   }
-    return null;
+
+  return null;
 });
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next ).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
