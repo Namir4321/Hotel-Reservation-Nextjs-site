beb9eab60a97484e38efd2591ab5f858028ce578
Namir4321 <97880446+Namir4321@users.noreply.github.com>
2024-08-21 22:26:00 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,4 @@
-export const publicRoutes = [
-  "/",
-  "/properties",
-  "/signin"
-];
-export const authRoutes = ["/signin", "/register"];
-export const apiAuthPrefix = ["/api/auth", ];
+export const publicRoutes = ["/", "/properties(?:/.*)?"];
+export const authRoutes = ["/signin", "/register",];
+export const apiAuthPrefix = ["/api/auth" ];
 export const DEFAULT_LOGIN_REDIRECT = "/";
