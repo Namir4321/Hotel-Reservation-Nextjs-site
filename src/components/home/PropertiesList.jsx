d90e5c3782c2db0c2d800b6cca99ee2f619c6f25
Namir4321 <97880446+Namir4321@users.noreply.github.com>
2024-08-24 02:10:40 +0530

    Update PropertiesList.jsx

--- src/components/home/PropertiesList.jsx
+++ src/components/home/PropertiesList.jsx
@@ -1,4 +1,4 @@
-import PropertyCard from "../card/PropertyCard";
+import PropertyCard from "@/components/card/PropertyCard";
 const PropertiesList = ({properties}) => {
   return (
     <section className="mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
