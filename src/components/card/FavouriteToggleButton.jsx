7a06e81a4fd6bb3c81c6c05b1ab2eda30f019c3e
Namir <nk.namirkhan1@gmail.com>
2024-08-24 19:22:04 +0530

    favourite change one

--- src/components/card/FavouriteToggleButton.jsx
+++ src/components/card/FavouriteToggleButton.jsx
@@ -4,9 +4,9 @@ import FavouriteToogleForm from "./FavouriteToogleForm";
 
 const FavouriteToggleButton = async ({ propertyId }) => {
   const userId = await getAuthUser();
-  if(!userId) return <CardSigninButton />
-  const favouriteId=await fetchFavouriteId({propertyId})
-  return <FavouriteToogleForm favouriteId={favouriteId} propertyId={propertyId}/>
+  // if(!userId) return <CardSigninButton />
+  // const favouriteId=await fetchFavouriteId({propertyId})
+  // return <FavouriteToogleForm favouriteId={favouriteId} propertyId={propertyId}/>
 };
 
 export default FavouriteToggleButton;
